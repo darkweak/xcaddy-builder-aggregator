e92652a84c4dc0e7b5704df090eb81b0815cd09c
darkweak <darkweak@protonmail.com>
2022-04-21 12:44:21 +0200

    Update deployer.go

--- deployer.go
+++ deployer.go
@@ -2,9 +2,10 @@ package aggregator
 
 import (
 	"fmt"
-	"github.com/google/go-github/v34/github"
 	"io/ioutil"
 	"log"
+
+	"github.com/google/go-github/v34/github"
 )
 
 func (c *CaddyAggregator) redeploy(data []byte) {
@@ -26,10 +27,11 @@ func (c *CaddyAggregator) pushToGit(data []byte) {
 			Name:  github.String(c.configuration.Owner),
 			Email: github.String(c.configuration.Email),
 		},
-
 		Content: data,
 	}
 
+	r, _, _, _ := c.restClient.Repositories.GetContents(c.clientCTX, c.configuration.Owner, c.configuration.Repository, c.configuration.Path, nil)
+	commitOption.SHA = r.SHA
 	x, _, err := c.restClient.Repositories.UpdateFile(c.clientCTX, c.configuration.Owner, c.configuration.Repository, c.configuration.Path, commitOption)
 	if err != nil {
 		panic(err)
